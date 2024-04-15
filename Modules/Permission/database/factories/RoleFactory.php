288bcc5798db2efef56e9c85eabbf579f883d0a0
AbdenourTadjer33 <tad.abdenour33@gmail.com>
2024-04-15 18:51:01 +0100

    Permission migration & factory & seed completed

--- Modules/Permission/database/factories/RoleFactory.php
+++ Modules/Permission/database/factories/RoleFactory.php
@@ -4,23 +4,22 @@ namespace Modules\Permission\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class PermissionFactory extends Factory
+class RoleFactory extends Factory
 {
     /**
      * The name of the factory's corresponding model.
      */
-    protected $model = \Modules\Permission\Models\Permission::class;
+    protected $model = \Modules\Permission\Models\Role::class;
 
     /**
      * Define the model's default state.
      */
     public function definition(): array
     {
-        $actions = ['view', 'create', 'edit', 'destroy', 'force-destroy'];
-        
         return [
-            'model' => fake()->word(),
-            'action' => $actions[rand(0, 4)],
+            'name' => fake()->word(),
+            'description' => fake()->sentence()
         ];
     }
 }
+
