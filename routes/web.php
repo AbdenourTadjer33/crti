ed0afe6f0e60deea7188a5bd1516c223ee666770
AbdenourTadjer33 <tad.abdenour33@gmail.com>
2024-04-25 14:23:31 +0100

    stable

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,3 @@
 <?php
 
-use App\Http\Controllers\TestController;
-use App\Models\User;
-use Modules\Permission\Models\Role;
 use Illuminate\Support\Facades\Route;
-use Modules\Permission\Models\Permission;
-
-Route::get('/', function () {
-    /** @var User */
-    $user = User::first();
-
-    // $user->can();
-});
-
-
-Route::get('/test/create', [TestController::class, 'create']);
