13b88f01f311adbfc410c38b5673b0dff1c762a2
AbdenourTadjer33 <tad.abdenour33@gmail.com>
2024-04-25 14:16:56 +0100

    stable

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use App\Models\User;
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +11,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
     }
 
     /**
@@ -21,17 +18,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Gate::before(function(User $user, $abilities) {
-            // dd($user, $abilities);
-            return false;
-        });
-
-        Gate::define('create', function (?User $user) {
-            return true;
-        });
-
-        Gate::define('update', function (?User $user) {
-            return true;
-        });
     }
 }
