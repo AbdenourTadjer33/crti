454fa83f9024b16c2d100e3a0f9d167f061f68c3
AbdenourTadjer33 <tad.abdenour33@gmail.com>
2024-04-30 21:11:57 +0100

    redirects path

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->redirectUsersTo('/app');
+        $middleware->redirectGuestsTo('/auth');
         $middleware->web([
             HandleInertiaRequests::class
         ]);
