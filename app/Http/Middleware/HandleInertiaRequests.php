0b6649f0f212be5e778748cd76f4b63bf6e2ae02
AbdenourTadjer33 <tad.abdenour33@gmail.com>
2024-04-30 21:09:34 +0100

    Inertia middleware

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -36,7 +36,8 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request): array
     {
         return array_merge(parent::share($request), [
-            //
+            'user' => fn () => $request->user()?->loadPermissions()?->loadRoles(),
+            'locale' => fn () => app()->getLocale(),
         ]);
     }
 }
