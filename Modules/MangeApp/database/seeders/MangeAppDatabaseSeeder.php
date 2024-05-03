f6244a79189e5b7ab09119e4a2b0be0a006e17c0
AbdenourTadjer33 <tad.abdenour33@gmail.com>
2024-05-03 18:45:07 +0100

    fresh module

---
+++ Modules/MangeApp/database/seeders/MangeAppDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\MangeApp\database\seeders;
+
+use Illuminate\Database\Seeder;
+
+class MangeAppDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
---
+++ Modules/MangeApp/resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('mangeapp::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>Module: {!! config('mangeapp.name') !!}</p>
+@endsection
