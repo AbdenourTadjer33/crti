3ce6cca00710dd05d1ca759eb977ed9a77ce1c56
AbdenourTadjer33 <tad.abdenour33@gmail.com>
2024-05-15 06:47:34 +0100

    stepper ready to use

--- resources/js/Components/Stepper.tsx
+++ resources/js/Components/Stepper.tsx
@@ -1,125 +1,99 @@
+import React from "react";
 import { cn } from "@/Utils/utils";
-import React, { useState } from "react";
-import { TiTick } from "react-icons/ti";
-import { Heading } from "./ui/heading";
 import { capitalize } from "@/Utils/helper";
-import { Text } from "./ui/paragraph";
 
-export type StepsData = (
-    | { label: string; description?: string; form?: () => {} }
-    | string
-)[];
-
-function Stepper({
-    stepper,
-    className,
-    ...props
-}: React.HTMLAttributes<HTMLOListElement> & {
-    stepper: { steps: StepsData; currentStep: number };
-}) {
-    return (
-        <ol
-            className={cn(
-                "items-center justify-center w-full space-y-4 sm:flex sm:space-x-8 sm:space-y-0",
-                className
-            )}
-            {...props}
-        >
-            {stepper.steps.map((step, idx) => {
-                return (
-                    <Step
-                        key={idx}
-                        idx={idx + 1}
-                        label={typeof step === "string" ? step : step?.label}
-                        description={
-                            typeof step === "string"
-                                ? undefined
-                                : step?.description
-                        }
-                        isActive={idx === stepper.currentStep}
-                        isError={false}
-                        isValid={false}
-                    />
-                );
-            })}
-        </ol>
-    );
-}
-
-function Step({
-    idx,
-    label,
-    description,
-    isActive,
-    isError,
-    isValid,
-    className,
-    ...props
-}: React.HTMLAttributes<HTMLLIElement> & {
-    idx: number;
+export type Step = {
     label: string;
-    description?: string;
-    isActive: boolean;
+    form: React.ReactNode;
     isError?: boolean;
-    isValid?: boolean;
-}) {
-    return (
-        <li
-            className={cn(
-                "flex items-center data-[active=true]:text-primary-600 data-[active=true]:dark:text-primary-500  text-gray-600 dark:text-gray-500 space-x-2.5 rtl:space-x-reverse"
-            )}
-            data-active={isActive}
-            {...props}
-        >
-            <span
-                className="flex items-center justify-center w-6 h-6 text-sm md:w-8 md:h-8 border data-[active=true]:border-primary-600 data-[active=true]:dark:border-primary-500 data-[active=true]:dark:text-primary-500 border-gray-600 dark:border-gray-400 rounded-full shrink-0"
-                data-active={isActive}
-            >
-                {idx}
-            </span>
-            <span>
-                <Heading
-                    level={5}
-                    className="text-sm md:text-base font-medium leading-tight"
-                >
-                    {capitalize(label)}
-                </Heading>
-                {description && (
-                    <Text className="text-sm md:text-base font-medium leading-tight">
-                        {capitalize(description)}
-                    </Text>
-                )}
-            </span>
-        </li>
+};
+
+interface StepperOptions {
+    steps: Step[];
+    state?: {
+        initial: number;
+    };
+}
+
+interface StepperHook {
+    currentStep: number;
+    setCurrentStep: React.Dispatch<React.SetStateAction<number>>;
+    canGoNext: boolean;
+    canGoPrev: boolean;
+    next: () => void;
+    prev: () => void;
+    goTo: (to: number) => void;
+    steps: Step[];
+}
+
+const useStepper = (options: StepperOptions): StepperHook => {
+    const finalSteps = React.useMemo(() => options.steps, [options.steps]);
+    const [step, setStep] = React.useState<number>(
+        options?.state?.initial ?? 0
     );
-}
 
-function FormStepper({ formStepper }) {
-    return formStepper.map((renderForm, idx) => (
-        <React.Fragment key={idx}>
-            {idx === step ? renderForm() : null}
-        </React.Fragment>
-    ));
-}
+    const stepsCount = finalSteps.length - 1;
 
-const useStepper = (_steps: StepsData, _current?: number) => {
-    const finalSteps = React.useMemo(() => _steps, [_steps]);
-    const [step, setStep] = React.useState<number>(_current ?? 0);
+    const canGoNext: boolean = step < stepsCount;
+
+    const canGoPrev: boolean = step > 0;
+
+    const next = () => setStep((_step) => (canGoNext ? _step + 1 : _step));
+
+    const prev = () => setStep((_step) => (canGoPrev ? _step - 1 : _step));
+
+    const goTo = (to: number) =>
+        setStep((_step) => (to >= 0 && to <= stepsCount ? to : _step));
 
     return {
-        stepper: { steps: finalSteps, currentStep: step },
-        // formStepper: finalSteps.map((step) =>
-        // typeof step !== "string" ? step.form : null
-        // ),
-        formStepper: finalSteps.map((step) => step.form),
-        step: step,
-        setStep: setStep,
-        next: () => setStep((prev) => prev + 1),
-        prev: () => setStep((prev) => prev - 1),
-        goTo: (to: number) => setStep(to),
-        canGoNext: step !== finalSteps.length - 1,
-        canGoPrev: step !== 0,
+        currentStep: step,
+        setCurrentStep: setStep,
+        canGoNext,
+        canGoPrev,
+        next,
+        prev,
+        goTo,
+        steps: finalSteps,
     };
 };
 
-export { useStepper, Stepper, FormStepper, Step };
+const Stepper = ({ stepper }: { stepper: StepperHook }) => {
+    return (
+        <React.Fragment>
+            <ol
+                className={cn(
+                    "items-center justify-center w-full space-y-4 sm:flex sm:space-x-8 sm:space-y-0"
+                )}
+                aria-label="stepper"
+            >
+                {stepper.steps.map((step, idx) => (
+                    <li
+                        key={idx}
+                        data-active={stepper.currentStep === idx}
+                        data-error={step.isError}
+                        className={cn(
+                            "flex items-center data-[active=true]:text-primary-600 data-[active=true]:dark:text-primary-500 data-[error=true]:text-red-600 data-[error=true]:dark:text-red-500 text-gray-600 dark:text-gray-500 space-x-2.5 rtl:space-x-reverse"
+                        )}
+                        onClick={() => stepper.goTo(idx)}
+                    >
+                        <span className="flex items-center justify-center w-6 h-6 text-sm md:w-8 md:h-8 border border-current rounded-full shrink-0">
+                            {idx + 1}
+                        </span>
+
+                        <h4 className={cn("text-sm md:text-base font-medium")}>
+                            {capitalize(step.label)}
+                        </h4>
+                    </li>
+                ))}
+            </ol>
+
+            {stepper.steps.map((step, idx) => (
+                <React.Fragment key={idx}>
+                    {stepper.currentStep === idx ? step.form : null}
+                </React.Fragment>
+            ))}
+        </React.Fragment>
+    );
+};
+
+export { useStepper, Stepper };
