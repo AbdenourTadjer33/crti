2691f87464a70396091f7db4169be6838bc45cad
AbdenourTadjer33 <tad.abdenour33@gmail.com>
2024-05-15 06:49:42 +0100

    code formatting

--- resources/js/Components/ui/input.tsx
+++ resources/js/Components/ui/input.tsx
@@ -8,20 +8,19 @@ export interface InputProps
     extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-    ({ className, type, ...props }, ref) => {
-        return (
-            <input
-                type={type}
-                className={cn(
-                    "flex h-10 w-full rounded-md border border-gray-300 bg-white text-gray-950 px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-500 focus:outline-none focus:ring-primary-600 focus:border-primary-600 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:dark:opacity-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder:text-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500",
-                    className
-                )}
-                ref={ref}
-                {...props}
-            />
-        );
-    }
+    ({ className, type, ...props }, ref) => (
+        <input
+            type={type}
+            className={cn(
+                "flex h-10 w-full rounded-md border border-gray-300 bg-white text-gray-950 px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-500 focus:outline-none focus:ring-primary-600 focus:border-primary-600 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:dark:opacity-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder:text-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500",
+                className
+            )}
+            ref={ref}
+            {...props}
+        />
+    )
 );
+
 Input.displayName = "Input";
 
 const InputError = ({
