3ed18454d1632225d6dadbd7591d913d6b4be3d6
AbdenourTadjer33 <tad.abdenour33@gmail.com>
2024-05-15 06:48:44 +0100

    FormWrapper component updated to replace the form element

--- resources/js/Components/ui/form.tsx
+++ resources/js/Components/ui/form.tsx
@@ -1,19 +1,30 @@
 import React from "react";
 import { cn } from "@/Utils/utils";
 
-const FormWrapper = React.forwardRef<
-    HTMLDivElement,
-    React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
-    <div
-        ref={ref}
-        className={cn(
-            "bg-white dark:bg-gray-800/30 p-4 border dark:border-gray-700 rounded-lg shadow-lg",
-            className
-        )}
-        {...props}
-    />
-));
+interface FormWrapperProps extends React.FormHTMLAttributes<HTMLFormElement> {
+    className?: string;
+}
+
+const FormWrapper = React.forwardRef<HTMLFormElement, FormWrapperProps>(
+    ({ className, onSubmit, ...props }, ref) => {
+        const submit = (event: React.FormEvent<HTMLFormElement>) => {
+            event.preventDefault();
+            if (onSubmit) onSubmit(event);
+        };
+
+        return (
+            <form
+                ref={ref}
+                className={cn(
+                    "bg-white dark:bg-gray-800/30 p-4 border dark:border-gray-700 rounded-lg shadow-lg",
+                    className
+                )}
+                onSubmit={submit}
+                {...props}
+            />
+        );
+    }
+);
 
 FormWrapper.displayName = "FormWrapper";
 
