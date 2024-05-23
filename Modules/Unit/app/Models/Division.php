85d97281f7987445602af8a45bdd5a9734e84815
AbdenourTadjer33 <tad.abdenour33@gmail.com>
2024-05-23 22:15:23 +0100

    Unit module: updating models

--- Modules/Unit/app/Models/Division.php
+++ Modules/Unit/app/Models/Division.php
@@ -2,21 +2,37 @@
 
 namespace Modules\Unit\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Modules\Unit\Database\Factories\DivisionFactory;
 
 class Division extends Model
 {
     use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     */
-    protected $fillable = [];
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        $this->table = 'divisions';
+    }
+    
+    protected $guarded = [];
 
     protected static function newFactory(): DivisionFactory
     {
         return DivisionFactory::new();
+    }
+
+    public function unit(): BelongsTo
+    {
+        return $this->belongsTo(Unit::class);
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class)->withTimestamps()->withPivot('grade');
     }
 }
