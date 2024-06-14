026eed0761a780a74c0b283ecee20d19019bd855
AbdenourTadjer33 <tad.abdenour33@gmail.com>
2024-06-14 02:27:46 +0100

    rename files

--- resources/js/Features/Project/Create/IdentificationStep.tsx
+++ resources/js/Features/Project/Create/IdentificationStep.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Input, InputError } from "@/Components/ui/input";
 import { Label } from "@/Components/ui/label";
 import { Popover } from "@/Components/ui/popover";
@@ -17,15 +17,28 @@ import {
 } from "@/Components/ui/command";
 import { CreateProjectContext } from "./Form";
 import { Button } from "@/Components/ui/button";
-import { CalendarIcon, Check, ChevronDown, ChevronUp } from "lucide-react";
+import { CalendarIcon, Check, ChevronDown } from "lucide-react";
 import { cn } from "@/Utils/utils";
 import { capitalize } from "@/Utils/helper";
 import { format } from "date-fns";
 import { useToast } from "@/Components/ui/use-toast";
 import { Calendar } from "@/Components/ui/calendar";
+import { FormProps } from "@/Components/Stepper";
 
-const Identification = () => {
-    const { data, setData, errors } = React.useContext(CreateProjectContext);
+const Identification = ({ next }: FormProps) => {
+    const { data, setData, errors, processing, validate, clearErrors } =
+        React.useContext(CreateProjectContext);
+
+    const goNext = async () => {
+        const toValidate =
+            "name,nature,domain,timeline,description,objectives,methodology,is_partner,partner,partner.name,partner.email,partner.phone";
+
+        validate(toValidate, {
+            onSuccess() {
+                next();
+            },
+        });
+    };
 
     return (
         <div className="grid grid-cols-3 gap-4">
@@ -33,27 +46,28 @@ const Identification = () => {
                 <Label>Intitulé du projet</Label>
                 <Input
                     value={data.name}
-                    onChange={(e) => setData("name", e.target.value)}
+                    onChange={(e) => {
+                        clearErrors("name");
+                        setData("name", e.target.value);
+                    }}
                 />
                 <InputError message={errors.name} />
             </div>
 
             <div className="space-y-1">
                 <Label>Nature du projet</Label>
-                <div>
-                    <ProjectNature />
-                </div>
+                <ProjectNature />
+                <InputError message={errors.nature} />
             </div>
 
             <div className="space-y-1">
                 <Label>Domaine d'application</Label>
-                <div>
-                    <DomainField />
-                </div>
+                <DomainField />
+                <InputError message={errors.domain} />
             </div>
 
             <div className="space-y-1">
-                <Label>Date de début/fin</Label>
+                <Label>Date début/fin</Label>
                 <Popover>
                     <PopoverTrigger asChild>
                         <Button
@@ -76,7 +90,10 @@ const Identification = () => {
                         <Calendar
                             mode="range"
                             selected={data.timeline}
-                            onSelect={(range) => setData("timeline", range)}
+                            onSelect={(range) => {
+                                clearErrors("timeline");
+                                setData("timeline", range);
+                            }}
                         />
                     </PopoverContent>
                 </Popover>
@@ -87,55 +104,118 @@ const Identification = () => {
                 <Label>Description succincte du projet</Label>
                 <Textarea
                     value={data.description}
-                    onChange={(e) => setData("description", e.target.value)}
+                    onChange={(e) => {
+                        clearErrors("description");
+                        setData("description", e.target.value);
+                    }}
                 />
                 <InputError message={errors.description} />
             </div>
 
             <div className="space-y-1 col-span-3">
                 <Label>Objectifs du projet</Label>
-
                 <Textarea
                     value={data.objectives}
-                    onChange={(e) => setData("objectives", e.target.value)}
+                    onChange={(e) => {
+                        clearErrors("objectives");
+                        setData("objectives", e.target.value);
+                    }}
                 />
+                <InputError message={errors.objectives} />
             </div>
 
             <div className="space-y-1 col-span-3">
                 <Label>Méthodologies pour la mise en œuvre du projet</Label>
                 <Textarea
                     value={data.methodology}
-                    onChange={(e) => setData("methodology", e.target.value)}
+                    onChange={(e) => {
+                        clearErrors("methodology");
+                        setData("methodology", e.target.value);
+                    }}
                 />
+                <InputError message={errors.methodology} />
             </div>
 
             <Label className="col-span-3 inline-flex gap-2">
                 <Checkbox
-                    defaultChecked={!!Object.keys(data.partner).length}
-                    onCheckedChange={(checked) =>
-                        setData(
-                            "partner",
-                            checked ? { name: "", resource: [] } : {}
-                        )
-                    }
+                    defaultChecked={data.is_partner}
+                    onCheckedChange={(checked) => {
+                        clearErrors("is_partner");
+                        if (!checked) {
+                            clearErrors(
+                                "partner.name",
+                                "partner.email",
+                                "partner.phone"
+                            );
+                        }
+                        setData("is_partner", !!checked);
+                    }}
                 />
                 Ajouter le partenaire socio-econimique
             </Label>
 
-            {!!Object.keys(data.partner).length && (
-                <div className="space-y-1 col-span-3">
-                    <Label>Nom du Partenair socio-economique</Label>
-                    <Input
-                        value={data.partner.name}
-                        onChange={(e) =>
-                            setData((data) => {
-                                data.partner.name = e.target.value;
-                                return { ...data };
-                            })
-                        }
-                    />
-                </div>
+            {data.is_partner && (
+                <>
+                    <div className="space-y-1">
+                        <Label>Nom du partenaire socio-economique</Label>
+                        <Input
+                            value={data.partner.name}
+                            onChange={(e) => {
+                                clearErrors("partner.name");
+                                setData((data) => {
+                                    data.partner.name = e.target.value;
+                                    return { ...data };
+                                });
+                            }}
+                        />
+                        <InputError message={errors["partner.name"]} />
+                    </div>
+
+                    <div className="space-y-1">
+                        <Label>Adresse e-mail du partenaire</Label>
+                        <Input
+                            value={data.partner.email}
+                            onChange={(e) => {
+                                clearErrors("partner.email");
+                                setData((data) => {
+                                    data.partner.email = e.target.value;
+                                    return { ...data };
+                                });
+                            }}
+                        />
+                        <InputError message={errors["partner.email"]} />
+                    </div>
+
+                    <div className="space-y-1">
+                        <Label>N° téléphone du partenaire</Label>
+                        <Input
+                            value={data.partner.phone}
+                            onChange={(e) => {
+                                clearErrors("partner.phone");
+                                setData((data) => {
+                                    data.partner.phone = e.target.value;
+                                    return { ...data };
+                                });
+                            }}
+                        />
+                        <InputError message={errors["partner.phone"]} />
+                    </div>
+                </>
             )}
+
+            <div className="col-span-3">
+                <div className="flex gap-4 max-w-lg mx-auto">
+                    <Button
+                        type="button"
+                        variant="secondary"
+                        className="w-full"
+                        disabled={processing}
+                        onClick={goNext}
+                    >
+                        Suivant
+                    </Button>
+                </div>
+            </div>
         </div>
     );
 };
@@ -148,7 +228,8 @@ const projectNatures = [
 ];
 
 const ProjectNature = () => {
-    const { data, setData, errors } = React.useContext(CreateProjectContext);
+    const { data, setData, clearErrors } =
+        React.useContext(CreateProjectContext);
     const [open, setOpen] = React.useState(false);
     const [search, setSearch] = React.useState("");
 
@@ -157,6 +238,7 @@ const ProjectNature = () => {
             return;
         }
         projectNatures.push(search);
+        clearErrors("nature");
         setData("nature", search);
         setOpen(false);
         setSearch("");
@@ -199,6 +281,7 @@ const ProjectNature = () => {
                                     key={idx}
                                     value={nature}
                                     onSelect={(currentValue) => {
+                                        clearErrors("nature");
                                         setData("nature", currentValue);
                                         setOpen(false);
                                     }}
@@ -251,10 +334,9 @@ const domains = [
 ];
 
 const DomainField = () => {
-    const { data, setData } = React.useContext(CreateProjectContext);
+    const { data, setData, clearErrors } =
+        React.useContext(CreateProjectContext);
     const [search, setSearch] = React.useState("");
-
-    const { toast } = useToast();
 
     const selectHandler = (value: string) => {
         const selectedDomains = [...data.domain];
@@ -265,6 +347,7 @@ const DomainField = () => {
             selectedDomains.push(value);
         }
 
+        clearErrors("domain");
         setData("domain", selectedDomains);
     };
 
