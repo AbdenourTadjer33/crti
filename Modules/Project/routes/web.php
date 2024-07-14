2c8eb3e1683f9fa68b3f02ed0ffa6e10a7c1b6ac
AbdenourTadjer33 <tad.abdenour33@gmail.com>
2024-07-14 05:06:01 +0100

    adding project.version resource & post route to sync the project version

--- Modules/Project/routes/web.php
+++ Modules/Project/routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Modules\Project\Http\Controllers\ProjectController;
+use Modules\Project\Http\Controllers\ProjectVersionController;
 
 Route::prefix('app')->middleware('auth')->group(function () {
-    Route::resource('project', ProjectController::class)->names('project');
+    Route::post('/projects/{project}/versions/sync', [ProjectVersionController::class, 'sync'])->name('project.version.sync');
+    Route::resource('projects', ProjectController::class)->only(["index", "store", "show"])->names('project');
+    Route::resource('projects.versions', ProjectVersionController::class)->names('project.version');
 });
