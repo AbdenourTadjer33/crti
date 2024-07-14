363604f5cd07d9a975a20fba7aa0235849d04ccf
AbdenourTadjer33 <tad.abdenour33@gmail.com>
2024-07-14 05:02:13 +0100

    excluding middleware on some routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,16 +12,23 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
-     )
+    )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->redirectGuestsTo('/');
+
         $middleware->redirectUsersTo(function () {
             return session('url.intended', '/app');
         });
-        $middleware->redirectGuestsTo('/');
+
         $middleware->web([
             HandleInertiaRequests::class
         ]);
+
         $middleware->statefulApi();
+
+        $middleware->convertEmptyStringsToNull([
+            fn (Request $request) => $request->is('app/projects/*/versions/sync'),
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
