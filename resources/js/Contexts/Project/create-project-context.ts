ed1b063bac0acdbeae74f89e9affa225a56f7a7c
AbdenourTadjer33 <tad.abdenour33@gmail.com>
2024-07-14 05:12:41 +0100

    create-project-context added

---
+++ resources/js/Contexts/Project/create-project-context.ts
@@ -0,0 +1,50 @@
+import React from "react";
+import { ValidationOptions } from "@/Libs/Validation";
+import { ProjectForm, setDataByKeyValuePair, setDataByMethod, setDataByObject, SetError } from "@/types/form";
+import { VisitOptions } from "@inertiajs/core";
+
+interface ProjectContext {
+    data: ProjectForm;
+    errors: Partial<Record<keyof ProjectForm | string, string>>;
+    processing: boolean;
+    setData: setDataByObject<ProjectForm> & setDataByMethod<ProjectForm> & setDataByKeyValuePair<ProjectForm>;
+    clearErrors: (...fields: (keyof ProjectForm)[]) => void;
+    setError: SetError<ProjectForm>;
+    validate: (fields: string, options?: Partial<ValidationOptions>) => void;
+    post: (url: string, options?: VisitOptions) => void;
+}
+
+const CreateProjectContext = React.createContext<ProjectContext>({
+    data: {
+        name: "",
+        nature: "",
+        domains: [],
+        timeline: { from: undefined, to: undefined },
+        description: "",
+        goals: "",
+        methodology: "",
+        is_partner: false,
+        partner: {
+            name: "",
+            email: "",
+            phone: "",
+        },
+        members: [],
+        resources: [],
+        resources_crti: [],
+        resources_partner: [],
+        tasks: [],
+    },
+    errors: {},
+    processing: false,
+    setData: () => { },
+    clearErrors: () => { },
+    setError: () => { },
+    validate: () => { },
+    post: () => { },
+});
+
+export {
+    type ProjectContext,
+    CreateProjectContext,
+}
