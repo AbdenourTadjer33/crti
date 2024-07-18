2525e5f5f16bb45c8dc7592c3fd73982f448eccf
AbdenourTadjer33 <tad.abdenour33@gmail.com>
2024-07-18 05:04:59 +0100

    Project feature

---
+++ lang/en/status.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    "creation" => "Creation in progress",
+    "new" => "New project",
+    "review" => "In review",
+    "pending" => "Pending",
+    "suspended" => "Suspended",
+    "rejected" => "Rejected",
+    "completed" => "Completed"
+];
