a2b4f9f88ef2c90007601d1db0e768c7be3ee6e4
AbdenourTadjer33 <tad.abdenour33@gmail.com>
2024-08-05 12:29:12 +0100

    project resources added

---
+++ app/Http/Resources/Project/ProjectMembersResource.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Resources\Project;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProjectMembersResource extends JsonResource
+{
+    public function toArray(Request $request)
+    {
+        return [
+            'uuid' => $this->uuid,
+            'name' => $this->first_name . ' ' . $this->last_name,
+            'email' => $this->email
+        ];
+    }
+}
---
+++ app/Http/Resources/Project/ProjectTasksResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources\Project;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProjectTasksResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'name' => $this->name,
+            'timeline' => [
+                'from' => $this->date_begin,
+                'to' => $this->date_end,
+            ],
+            'description' => $this->description,
+            'result' => $this->result,
+            'priority' => $this->priority,
+            'assigned' => ProjectMembersResource::collection($this->whenLoaded('users')),
+        ];
+    }
+}
