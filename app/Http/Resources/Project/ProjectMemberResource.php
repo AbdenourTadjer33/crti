75bcaa8b08ab44039b1bbaa0b362abf850dbadf7
AbdenourTadjer33 <abdennour.tadjer@gmail.com>
2024-08-11 14:20:55 +0100

    renamed file

--- app/Http/Resources/Project/ProjectMemberResource.php
+++ app/Http/Resources/Project/ProjectMemberResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources\Project;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class ProjectMembersResource extends JsonResource
+class ProjectMemberResource extends JsonResource
 {
     public function toArray(Request $request)
     {
