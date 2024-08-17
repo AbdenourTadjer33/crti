f75a036ddc93e23d7ed73b72981dac18b40e8385
AbdenourTadjer33 <abdennour.tadjer@gmail.com>
2024-08-17 20:46:26 +0100

    adding fields

--- resources/js/Features/Project/Version/Create/IdentificationStep.tsx
+++ resources/js/Features/Project/Version/Create/IdentificationStep.tsx
@@ -295,4 +295,344 @@ const Identification = ({ next }: FormProps) => {
     );
 };
 
+interface NatureFieldProps {
+    value: string;
+    setValue: (value: string) => void;
+    natures: undefined | string[];
+}
+
+const NatureField: React.FC<NatureFieldProps> = ({
+    value,
+    setValue,
+    natures,
+}) => {
+    const [data, setData] = React.useState(natures);
+    const [open, setOpen] = React.useState(false);
+    const [search, setSearch] = React.useState("");
+    const [loading, setLoading] = React.useState(false);
+
+    const addNewItem = async () => {
+        const item = search.trim();
+        const response = await addProjectNature(item);
+
+        if (response?.status === 201) {
+            setData((prev) => {
+                return [...(prev as []), item];
+            });
+            return;
+        }
+
+        if (response?.status === 422) {
+            alert(response.data.message);
+        }
+    };
+
+    useUpdateEffect(() => setData(natures), [natures]);
+
+    return (
+        <Popover.Popover open={open} onOpenChange={setOpen}>
+            <Popover.PopoverTrigger asChild>
+                <Button
+                    variant="outline"
+                    className="w-full justify-between pr-0"
+                    onClick={() => {
+                        if (!natures) {
+                            router.reload({
+                                only: ["natures"],
+                                onStart: (e) => setLoading(true),
+                                onFinish: (e) => setLoading(false),
+                            });
+                        }
+                    }}
+                >
+                    <div className="w-full truncate text-start">
+                        {value || "Sélectionner la nature de projet"}
+                    </div>
+
+                    <div
+                        className={buttonVariants({
+                            variant: "ghost",
+                            size: "sm",
+                        })}
+                    >
+                        <ChevronDown className="shrink-0 h-4 w-4" />
+                    </div>
+                </Button>
+            </Popover.PopoverTrigger>
+            <Popover.PopoverContent className="w-auto p-0">
+                <Command.Command loop>
+                    <Command.CommandHeader>
+                        <Command.CommandInput
+                            value={search}
+                            onValueChange={(search) =>
+                                setSearch(capitalize(search))
+                            }
+                            placeholder="Rechercher ou Ajouter..."
+                        />
+                        <div>
+                            <Button
+                                variant="ghost"
+                                size="icon"
+                                onClick={addNewItem}
+                            >
+                                <Plus className="shrink-0 h-5 w-5" />
+                            </Button>
+                        </div>
+                    </Command.CommandHeader>
+                    <Command.CommandList>
+                        {loading ? (
+                            <Command.CommandLoading className="flex items-center justify-center gap-2">
+                                <LoaderCircle className="h-4 w-4 animate-spin" />
+                                Chargement...
+                            </Command.CommandLoading>
+                        ) : (
+                            <Command.CommandEmpty className="p-4">
+                                {!search ? (
+                                    "No results found."
+                                ) : (
+                                    <Button
+                                        variant="ghost"
+                                        className="text-sm gap-1"
+                                        onClick={addNewItem}
+                                    >
+                                        Ajouter{" "}
+                                        <span className="font-medium">
+                                            {capitalize(search)}
+                                        </span>
+                                    </Button>
+                                )}
+                            </Command.CommandEmpty>
+                        )}
+
+                        <Command.CommandGroup>
+                            {data &&
+                                data.map((nature, idx) => (
+                                    <Command.CommandItem
+                                        key={idx}
+                                        value={nature}
+                                        onSelect={(currentValue) => {
+                                            setValue(
+                                                currentValue === value
+                                                    ? ""
+                                                    : currentValue
+                                            );
+                                            setOpen(false);
+                                            setSearch("");
+                                        }}
+                                    >
+                                        <Check
+                                            className={cn(
+                                                "mr-2 h-4 w-4",
+                                                value === nature
+                                                    ? "opacity-100"
+                                                    : "opacity-0"
+                                            )}
+                                        />
+                                        {nature}
+                                    </Command.CommandItem>
+                                ))}
+                        </Command.CommandGroup>
+                    </Command.CommandList>
+                </Command.Command>
+            </Popover.PopoverContent>
+        </Popover.Popover>
+    );
+};
+
+interface DomainFieldProps {
+    values: string[];
+    setValues: (values: string[]) => void;
+    domains: undefined | string[];
+}
+
+const DomainField: React.FC<DomainFieldProps> = ({
+    values,
+    setValues,
+    domains,
+}) => {
+    const [data, setData] = React.useState(domains);
+    const [open, setOpen] = React.useState(false);
+    const [search, setSearch] = React.useState("");
+    const [loading, setLoading] = React.useState(false);
+
+    const addNewItem = async () => {
+        const item = search.trim();
+        const response = await addProjectDomain(item);
+
+        if (response?.status === 201) {
+            setData((prev) => {
+                return [...(prev as []), item];
+            });
+            return;
+        }
+
+        if (response?.status === 422) {
+            alert(response.data.message);
+        }
+    };
+
+    useUpdateEffect(() => setData(domains), [domains]);
+
+    return (
+        <Popover.Popover open={open} onOpenChange={setOpen}>
+            <Popover.PopoverTrigger asChild>
+                <Button
+                    variant="outline"
+                    className="w-full justify-between pr-0"
+                    onClick={() => {
+                        if (!domains) {
+                            router.reload({
+                                only: ["domains"],
+                                onStart: () => setLoading(true),
+                                onFinish: () => setLoading(false),
+                            });
+                        }
+                    }}
+                >
+                    <div className="w-full truncate text-start">
+                        {values.join(", ")}
+                    </div>
+                    <div
+                        className={buttonVariants({
+                            variant: "ghost",
+                            size: "sm",
+                        })}
+                    >
+                        <ChevronDown className="shrink-0 h-4 w-4" />
+                    </div>
+                </Button>
+            </Popover.PopoverTrigger>
+            <Popover.PopoverContent className="w-auto p-0">
+                <Command.Command loop>
+                    <Command.CommandHeader>
+                        <Command.CommandInput
+                            value={search}
+                            onValueChange={(search) =>
+                                setSearch(capitalize(search))
+                            }
+                            placeholder="Rechercher ou Ajouter..."
+                        />
+                        <div>
+                            <Button
+                                variant="ghost"
+                                size="icon"
+                                onClick={addNewItem}
+                            >
+                                <Plus className="shrink-0 h-5 w-5" />
+                            </Button>
+                        </div>
+                    </Command.CommandHeader>
+                    <Command.CommandList>
+                        {loading ? (
+                            <Command.CommandLoading className="flex items-center justify-center gap-2">
+                                <LoaderCircle className="h-4 w-4 animate-spin" />
+                                Chargement...
+                            </Command.CommandLoading>
+                        ) : (
+                            <Command.CommandEmpty className="p-4">
+                                {!search ? (
+                                    "No results found."
+                                ) : (
+                                    <Button
+                                        variant="ghost"
+                                        className="text-sm gap-1"
+                                        onClick={addNewItem}
+                                    >
+                                        Ajouter{" "}
+                                        <span className="font-medium">
+                                            {search}
+                                        </span>
+                                    </Button>
+                                )}
+                            </Command.CommandEmpty>
+                        )}
+
+                        <Command.CommandGroup>
+                            {data &&
+                                data.map((domain, idx) => (
+                                    <Command.CommandItem
+                                        key={idx}
+                                        value={domain}
+                                        onSelect={() => {
+                                            const selected = [...values];
+
+                                            if (values.includes(domain)) {
+                                                selected.splice(
+                                                    selected.indexOf(domain),
+                                                    1
+                                                );
+                                            } else {
+                                                selected.push(domain);
+                                            }
+
+                                            setValues(selected);
+                                        }}
+                                    >
+                                        <Check
+                                            className={
+                                                "mr-2 h-4 w-4 data-[checked=false]:opacity-0"
+                                            }
+                                            data-checked={values?.includes(
+                                                domain
+                                            )}
+                                        />
+                                        {domain}
+                                    </Command.CommandItem>
+                                ))}
+                        </Command.CommandGroup>
+                    </Command.CommandList>
+                </Command.Command>
+            </Popover.PopoverContent>
+        </Popover.Popover>
+    );
+};
+
+interface TimelineFieldProps {
+    value: DateRange;
+    setValue: (value: DateRange | undefined) => void;
+}
+
+const TimelineField: React.FC<TimelineFieldProps> = ({ value, setValue }) => {
+    console.log(value);
+    return (
+        <Popover.Popover>
+            <Popover.PopoverTrigger asChild>
+                <Button
+                    variant="outline"
+                    className="w-full justify-between pr-0"
+                >
+                    <div className="w-full truncate text-start">
+                        {!value || (!value?.from && !value?.to)
+                            ? "Date début/fin"
+                            : value.from && !value.to
+                            ? `De ${format(value.from, "dd/MM/yyy")}`
+                            : value.from &&
+                              value.to &&
+                              `De ${format(value.from, "dd/MM/yyy")} à ${format(
+                                  value.to,
+                                  "dd/MM/yyy"
+                              )}`}
+                    </div>
+                    <div
+                        className={buttonVariants({
+                            variant: "ghost",
+                            size: "sm",
+                        })}
+                    >
+                        <CalendarIcon className="shrink-0 w-5 h-5 text-gray-500 dark:text-gray-400" />
+                    </div>
+                </Button>
+            </Popover.PopoverTrigger>
+            <Popover.PopoverContent className="w-auto p-0">
+                <Calendar
+                    mode="range"
+                    showOutsideDays={false}
+                    selected={value}
+                    onSelect={setValue}
+                />
+            </Popover.PopoverContent>
+        </Popover.Popover>
+    );
+};
+
 export default Identification;
