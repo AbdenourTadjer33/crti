2f3cafb2a1ee05162c73b007a7ee49f45c7e16aa
AbdenourTadjer33 <abdennour.tadjer@gmail.com>
2024-08-19 02:23:14 +0100

    update title and description

--- resources/js/Pages/Project/Create.tsx
+++ resources/js/Pages/Project/Create.tsx
@@ -7,26 +7,34 @@ import { Text } from "@/Components/ui/paragraph";
 import Breadcrumb from "@/Components/Breadcrumb";
 import Form from "@/Features/Project/Version/Create/Form";
 
-const breadcrubms = [
-    { href: route("app"), label: <MdHome className="w-6 h-6" /> },
-    { href: route("project.index"), label: "Projets" },
-    { label: "Continue la création de projet" },
-];
-
 const create: React.FC<any> = ({ version }) => {
+    const breadcrubms = React.useMemo(() => [
+        { href: route("app"), label: <MdHome className="w-6 h-6" /> },
+        { href: route("project.index"), label: "Projets" },
+        { label: "Commencez Votre Projet" },
+    ], []);
+
     return (
         <AuthLayout>
-            <Head title="Continue la création de projet" />
+            <Head title="Commencez Votre Projet" />
             <div className="space-y-4">
                 <Breadcrumb items={breadcrubms} />
 
                 <div className="flex sm:flex-row flex-col justify-between sm:items-end gap-4">
                     <div className="space-y-2">
                         <Heading level={3} className="font-medium">
-                            Continue la création de projet
+                            Commencez Votre Projet
                         </Heading>
-                        <Text className={"max-w-7xl"}>
-                            Votre modèle de tableau de bord de gestion d'accées.
+                        <Text>
+                            Votre projet a été initialisé avec succès.
+                            Maintenant, commençons à le construire ! Cette page
+                            est votre espace de travail pour entrer tous les
+                            détails importants de votre projet. Prenez le temps
+                            de remplir les informations nécessaires. Si vous
+                            devez faire une pause, ne vous inquiétez pas—votre
+                            progression sera sauvegardée, et vous pourrez
+                            revenir à tout moment pour continuer là où vous vous
+                            êtes arrêté.
                         </Text>
                     </div>
                 </div>
