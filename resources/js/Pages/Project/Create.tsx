b8db22d1963a4ca857da73d39125d25a0a59fd09
AbdenourTadjer33 <abdennour.tadjer@gmail.com>
2024-08-19 15:07:29 +0100

    adding detection if is first time that user visit the page and displaying content based on this condition

--- resources/js/Pages/Project/Create.tsx
+++ resources/js/Pages/Project/Create.tsx
@@ -8,25 +8,43 @@ import Breadcrumb from "@/Components/Breadcrumb";
 import Form from "@/Features/Project/Version/Create/Form";
 
 const create: React.FC<any> = ({ version }) => {
-    const breadcrubms = React.useMemo(() => [
-        { href: route("app"), label: <MdHome className="w-6 h-6" /> },
-        { href: route("project.index"), label: "Projets" },
-        { label: "Commencez Votre Projet" },
-    ], []);
+    const isStarting = React.useMemo(() => !version, []);
+
+    const breadcrubms = React.useMemo(
+        () => [
+            { href: route("app"), label: <MdHome className="w-6 h-6" /> },
+            { href: route("project.index"), label: "Projets" },
+            {
+                label: isStarting
+                    ? "Commencez Votre Projet"
+                    : "Continuez Votre Projet",
+            },
+        ],
+        []
+    );
 
     return (
         <AuthLayout>
-            <Head title="Commencez Votre Projet" />
+            <Head
+                title={
+                    isStarting
+                        ? "Commencez Votre Projet"
+                        : "Continuez Votre Projet"
+                }
+            />
             <div className="space-y-4">
                 <Breadcrumb items={breadcrubms} />
 
                 <div className="flex sm:flex-row flex-col justify-between sm:items-end gap-4">
                     <div className="space-y-2">
                         <Heading level={3} className="font-medium">
-                            Commencez Votre Projet
+                            {isStarting
+                                ? "Commencez Votre Projet"
+                                : "Continuez Votre Projet"}
                         </Heading>
                         <Text>
-                            Votre projet a été initialisé avec succès.
+                            {isStarting
+                                ? `Votre projet a été initialisé avec succès.
                             Maintenant, commençons à le construire ! Cette page
                             est votre espace de travail pour entrer tous les
                             détails importants de votre projet. Prenez le temps
@@ -34,7 +52,19 @@ const create: React.FC<any> = ({ version }) => {
                             devez faire une pause, ne vous inquiétez pas—votre
                             progression sera sauvegardée, et vous pourrez
                             revenir à tout moment pour continuer là où vous vous
-                            êtes arrêté.
+                            êtes arrêté.`
+                                : `Vous êtes de retour ! Continuez à travailler sur
+                            votre projet là où vous vous étiez arrêté. Cette
+                            page est conçue pour vous aider à finaliser votre
+                            projet en vous permettant de revoir, mettre à jour
+                            et compléter toutes les sections nécessaires. Si
+                            vous vous étiez précédemment interrompu lors de la
+                            création initiale, reprenez simplement là où vous
+                            vous étiez arrêté. Votre projet est sauvegardé et
+                            vous attend dans l'index des projets—cliquez
+                            simplement dessus pour reprendre. Une fois que tout
+                            est en place, finalisez votre projet et soumettez-le
+                            pour révision.`}
                         </Text>
                     </div>
                 </div>
