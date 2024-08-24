cd5353b174cf127abfcb87b29af1b6328008366f
AbdenourTadjer33 <abdennour.tadjer@gmail.com>
2024-08-24 15:05:57 +0100

    confirmation step

--- resources/js/Features/Project/Version/Create/ConfirmationStep.tsx
+++ resources/js/Features/Project/Version/Create/ConfirmationStep.tsx
@@ -1,10 +1,23 @@
-import { FormProps } from "@/Components/Stepper";
 import React from "react";
+import * as Card from "@/Components/ui/card";
+import TanstackTable from "@tanstack/react-table";
+import DataTable from "@/Components/common/data-table";
+import { StepperContentProps } from "@/Components/ui/stepper";
 import { CreateProjectContext as ProjectContext } from "@/Contexts/Project/create-project-context";
 import { Button } from "@/Components/ui/button";
 import { router } from "@inertiajs/react";
 import { format } from "date-fns";
 import { ProjectForm } from "@/types/form";
+import { Text } from "@/Components/ui/paragraph";
+import { Heading } from "@/Components/ui/heading";
+import { Badge } from "@/Components/ui/badge";
+import { Avatar, AvatarFallback } from "@/Components/ui/avatar";
+import { currencyFormat, getInitials } from "@/Utils/helper";
+import { useUser } from "@/Hooks/use-user";
+import { Editor } from "@/Components/Editor";
+import { columnDef } from "../task-columns";
+import { columnDef as resourceColumnDef } from "../resource-columns";
+import { TableWrapper } from "@/Components/ui/table";
 
 function prepareData(data: Omit<ProjectForm, "creator">) {
     const formData: any = { ...data };
@@ -20,8 +33,9 @@ function prepareData(data: Omit<ProjectForm, "creator">) {
     return formData;
 }
 
-const ConfirmationStep = ({ prev }: FormProps) => {
-    const { data, errors, processing } = React.useContext(ProjectContext);
+const ConfirmationStep = ({ prev }: StepperContentProps) => {
+    const { uuid } = useUser("uuid");
+    const { data, processing } = React.useContext(ProjectContext);
 
     const submitHandler = () => {
         const formData = prepareData(data);
@@ -35,8 +49,226 @@ const ConfirmationStep = ({ prev }: FormProps) => {
     };
 
     return (
-        <div className="space-y-8 overflow-hidden">
-            <pre>{JSON.stringify({ data, errors }, null, 2)}</pre>
+        <>
+            <Card.Card>
+                <Card.CardHeader>
+                    <Text></Text>
+                    <Card.CardTitle className="sm:text-2xl text-xl">
+                        {data.name}
+                    </Card.CardTitle>
+                    <Card.CardSubTitle className="sm:text-xl text-lg">
+                        {data.nature}
+                    </Card.CardSubTitle>
+                </Card.CardHeader>
+                <Card.CardContent className="space-y-4 sm:space-y-6">
+                    <div className="space-y-2">
+                        <Heading level={6}>Domaines</Heading>
+                        <div className="flex flex-wrap items-center gap-1 5">
+                            {data.domains.map((domain, idx) => (
+                                <Badge key={idx} variant="blue">
+                                    {domain}
+                                </Badge>
+                            ))}
+                        </div>
+                    </div>
+
+                    <div className="space-y-2">
+                        <Heading level={6}>Chronologie</Heading>
+                        <div className="text-gray-600">
+                            {`Du ${format(
+                                data.timeline.from!,
+                                "dd MMMM yyy"
+                            )} au ${format(data.timeline.to!, "dd MMMM yyy")}`}
+                        </div>
+                    </div>
+
+                    <div className="flex md:flex-row flex-col md:items-start gap-4">
+                        <Card.Card className="flex-1 md:max-w-md w-full">
+                            <Card.CardHeader>
+                                <Card.CardTitle className="sm:text-2xl text-xl">
+                                    Membres de l'équipe de projet
+                                </Card.CardTitle>
+                                <Card.CardDescription>
+                                    Les membres de votre équipe.
+                                </Card.CardDescription>
+                            </Card.CardHeader>
+                            <Card.CardContent className="grid gap-6">
+                                {data.members.map((member) => (
+                                    <div
+                                        key={member.uuid}
+                                        className="flex items-center justify-between space-x-4"
+                                    >
+                                        <div className="flex items-center space-x-4">
+                                            <Avatar>
+                                                <AvatarFallback>
+                                                    {getInitials(member.name)}
+                                                </AvatarFallback>
+                                            </Avatar>
+                                            <div>
+                                                <p className="text-sm font-medium leading-none">
+                                                    {member.name}{" "}
+                                                    {member.uuid === uuid && (
+                                                        <span className="font-semibold">
+                                                            (Porteur)
+                                                        </span>
+                                                    )}
+                                                </p>
+                                                <p className="text-sm text-gray-600">
+                                                    {member.email}
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}
+                            </Card.CardContent>
+                        </Card.Card>
+
+                        {data.is_partner && (
+                            <Card.Card className="flex-1 md:max-w-md w-full">
+                                <Card.CardHeader>
+                                    <Card.CardTitle className="sm:text-2xl text-xl">
+                                        Partenaire socio-économique
+                                    </Card.CardTitle>
+                                    <Card.CardDescription>
+                                        Information sur le partenaire
+                                        socioéconomique
+                                    </Card.CardDescription>
+                                </Card.CardHeader>
+                                <Card.CardContent className="space-y-4">
+                                    <div className="space-y-1">
+                                        <Text>
+                                            Organisation:{" "}
+                                            <span className="text-gray-800 font-medium">
+                                                {data.partner.organisation}
+                                            </span>
+                                        </Text>
+                                        <Text>
+                                            Secteur:{" "}
+                                            <span className="text-gray-800 font-medium">
+                                                {data.partner.sector}
+                                            </span>
+                                        </Text>
+                                    </div>
+                                    <div className="space-y-1">
+                                        <Text>
+                                            Nom du contact:{" "}
+                                            <span className="text-gray-800 font-medium">
+                                                {data.partner.contact_name}
+                                            </span>
+                                        </Text>
+                                        <Text>
+                                            Post du contact:{" "}
+                                            <span className="text-gray-800 font-medium">
+                                                {data.partner.contact_post}
+                                            </span>
+                                        </Text>
+                                        <Text>
+                                            N° téléphone:{" "}
+                                            <span className="text-gray-800 font-medium">
+                                                {data.partner.contact_phone}
+                                            </span>
+                                        </Text>
+                                        <Text>
+                                            Adresse e-mail:{" "}
+                                            <span className="text-gray-800 font-medium">
+                                                {data.partner.contact_email}
+                                            </span>
+                                        </Text>
+                                    </div>
+                                </Card.CardContent>
+                            </Card.Card>
+                        )}
+                    </div>
+
+                    <div className="space-y-2">
+                        <Heading level={6}>
+                            Description succincte du projet
+                        </Heading>
+
+                        <Editor
+                            autofocus={false}
+                            content={data.description}
+                            editable={false}
+                            classNames={{ content: "resize-none" }}
+                        />
+                    </div>
+
+                    <div className="space-y-2">
+                        <Heading level={6}>Objectifs du projet</Heading>
+                        <Editor
+                            autofocus={false}
+                            content={data.goals}
+                            editable={false}
+                            classNames={{ content: "resize-none" }}
+                        />
+                    </div>
+
+                    <div className="space-y-2">
+                        <Heading level={6}>
+                            Méthodologies pour la mise en œuvre du projet
+                        </Heading>
+                        <Editor
+                            autofocus={false}
+                            content={data.methodology}
+                            editable={false}
+                            classNames={{ content: "resize-none" }}
+                        />
+                    </div>
+
+                    <div className="space-y-2">
+                        <Heading level={6}>Tâches</Heading>
+                        <TaskTable tasks={data.tasks} />
+                    </div>
+
+                    <div className="space-y-2">
+                        <Heading level={6}>
+                            Matériel existant pouvant être utilisé dans
+                            l'exécution du projet.
+                        </Heading>
+                        <div>Pas de ressources séléctionne</div>
+                    </div>
+
+                    <div className="space-y-2">
+                        <Heading level={6}>
+                            Matière première, composants et petits équipements à
+                            acquérir par le CRTI
+                        </Heading>
+
+                        <ResourceTable resources={data.resources_crti} />
+                    </div>
+
+                    <div className="space-y-2">
+                        <Heading level={6}>
+                            Matière première, composants et petits équipements à
+                            acquérir par le partenaire socio-économique
+                        </Heading>
+
+                        <ResourceTable resources={data.resources_partner} />
+                    </div>
+
+                    <Card.Card className="p-6 space-y-6">
+                        <div className="space-y-2">
+                            <Heading level={6}>
+                                Produit livrable du projet
+                            </Heading>
+                            <ul className="list-disc list-inside">
+                                {data.deliverables.map((deliverable, idx) => (
+                                    <li key={idx}>{deliverable}</li>
+                                ))}
+                            </ul>
+                        </div>
+                        <div className="inline-flex items-center gap-2">
+                            <Heading level={6} className="font-medium">
+                                Montant Globale estimé pour la réalisation du
+                                projet
+                            </Heading>
+                            <span className="text-gray-800 text-2xl">
+                                {currencyFormat(data.estimated_amount)}
+                            </span>
+                        </div>
+                    </Card.Card>
+                </Card.CardContent>
+            </Card.Card>
 
             <div className="flex gap-4 max-w-lg mx-auto">
                 <Button
@@ -50,6 +282,7 @@ const ConfirmationStep = ({ prev }: FormProps) => {
                 </Button>
                 <Button
                     type="button"
+                    variant="primary"
                     className="w-full"
                     disabled={processing}
                     onClick={submitHandler}
@@ -57,7 +290,80 @@ const ConfirmationStep = ({ prev }: FormProps) => {
                     Confirmer
                 </Button>
             </div>
-        </div>
+        </>
+    );
+};
+
+const TaskTable: React.FC<any> = ({ tasks }) => {
+    const columns = React.useMemo(() => columnDef, [columnDef]);
+    const data = React.useMemo(() => tasks, [tasks]);
+
+    const table = TanstackTable.useReactTable({
+        data,
+        columns,
+        getCoreRowModel: TanstackTable.getCoreRowModel(),
+        getRowId: (_, index) => String(index + 1),
+        getRowCanExpand: () => true,
+        getExpandedRowModel: TanstackTable.getExpandedRowModel(),
+    });
+
+    return (
+        <TableWrapper className="shadow-none">
+            <DataTable
+                options={{
+                    table,
+                    subComponent: ({ row }: any) => {
+                        return (
+                            <Editor
+                                editable={false}
+                                content={row.original.description}
+                                autofocus={false}
+                                classNames={{ content: "resize-none" }}
+                                className="-m-4 border-none"
+                            />
+                        );
+                    },
+                }}
+            />
+        </TableWrapper>
+    );
+};
+
+const ResourceTable: React.FC<any> = ({ resources }) => {
+    const columns = React.useMemo(() => resourceColumnDef, [resourceColumnDef]);
+    const data = React.useMemo(() => resources, [resources]);
+
+    const table = TanstackTable.useReactTable({
+        data,
+        columns,
+        getCoreRowModel: TanstackTable.getCoreRowModel(),
+        getRowId: (_, index) => String(index + 1),
+        getRowCanExpand: () => true,
+        getExpandedRowModel: TanstackTable.getExpandedRowModel(),
+    });
+
+    return (
+        <TableWrapper className="shadow-none">
+            <DataTable
+                options={{
+                    table,
+                    subComponent: ({ row }: any) => (
+                        <Editor
+                            editable={false}
+                            autofocus={false}
+                            content={row.original.description}
+                            classNames={{ content: "resize-none" }}
+                            className="-m-4 border-none"
+                        />
+                    ),
+                    noDataPlaceholder: (
+                        <div className="text-center text-xl font-medium">
+                            Pas de ressources ajouté
+                        </div>
+                    ),
+                }}
+            />
+        </TableWrapper>
     );
 };
 
