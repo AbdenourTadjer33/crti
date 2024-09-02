0f3cae1c6feaaa08eae6bbaa140652d46adec74b
AbdenourTadjer33 <abdennour.tadjer@gmail.com>
2024-09-02 09:08:17 +0100

    deleted

--- Modules/Authentification/lang/en/auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'failed' => 'The provided credentials are incorrect.',
-    'disconnected' => 'You have been disconnected successfully',
-    'token.deleted' => 'the token has been deleted successfully',
-    'tokens.deleted' => 'All sessions are closed',
-    'inactive' => 'Your account is not active',
-];
--- Modules/Authentification/lang/fr/auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'failed' => 'Les informations d\'identification fournies sont incorrectes.',
-    'disconnected' => 'You have been disconnected successfully',
-    'token.deleted' => 'the token has been deleted successfully',
-    'tokens.deleted' => 'All sessions are closed',
-    'inactive' => 'Your account is not active',
-];
