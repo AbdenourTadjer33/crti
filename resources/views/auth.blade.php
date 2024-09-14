bbe9a759763992e66d7ce1fb54f7475c87b76443
AbdenourTadjer33 <abdennour.tadjer@gmail.com>
2024-09-14 23:53:41 +0100

    views

--- resources/views/auth.blade.php
+++ resources/views/auth.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,20 +11,35 @@
     {{-- <link rel="preconnect" href="https://fonts.bunny.net"> --}}
     {{-- <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" /> --}}
 
-    {{-- <script>
-        if (JSON.parse(localStorage.getItem('color-theme')) === 'dark' || (!('color-theme' in localStorage) && window
-                .matchMedia('(prefers-color-scheme: dark)').matches)) {
-            document.documentElement.classList.add('dark');
-        } else {
-            document.documentElement.classList.remove('dark');
-        }
-    </script> --}}
+    <script>
+        ! function() {
+            try {
+                const root = document.documentElement;
+
+                let e = JSON.parse(localStorage.getItem('color-theme'));
+
+                if (!e) {
+                    e = 'system';
+                    localStorage.setItem('color-theme', JSON.stringify(e));
+                }
+
+                if (e === 'dark' || (e === 'system' && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+                    root.classList.add('dark');
+                    return;
+                }
+
+                root.classList.remove('dark');
+            } catch (e) {}
+        }()
+    </script>
 
     @viteReactRefresh
     @vite(['resources/js/app.tsx', "resources/js/Pages/{$page['component']}.tsx"])
     @inertiaHead
 </head>
+
 <body class="font-sans antialiased bg-gray-50 dark:bg-gray-900 text-gray-800 dark:text-gray-50">
     @inertia
 </body>
+
 </html>
