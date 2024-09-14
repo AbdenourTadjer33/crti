ae25045ee49e4c73866ef876b4127e8044a9a846
Reda Benabbas <redabenabbas8@gmail.com>
2024-09-14 21:25:23 +0100

    welcome page

---
+++ resources/views/components/hero.blade.php
@@ -0,0 +1,53 @@
+<div class="container px-6 py-16 mx-auto gap-">
+    <div class="items-center lg:flex">
+        <div class="w-full lg:w-1/2">
+            <div class="lg:max-w-lg">
+                <h1 class="text-3xl font-semibold text-gray-800 dark:text-white lg:text-4xl">Optimisez la Gestion de Vos Projets de Recherche</h1>
+                <p class="mt-3 text-gray-600 dark:text-gray-400 text-justify">
+                    Bienvenue sur l'application de gestion de projet de recherche
+                    dédiée aux chercheurs, administrateurs et institutions.
+                    Notre solution vous permet de collaborer facilement,
+                    de suivre l'avancement de vos projets et de centraliser
+                    les informations essentielles pour une gestion simplifiée et efficace.
+                <p>
+            </div>
+        </div>
+        <div class="flex items-center justify-center w-full mt-6 lg:mt-0 lg:w-1/2">
+            <img class="w-full h-full rounded-lg lg:max-w-3xl" src="assets/portrait-engineers-work-hours-job-site.jpg" alt="Catalogue-pana.svg">
+        </div>
+    </div>
+</div>
+
+<div class="bg-white dark:bg-gray-900">
+    <div class="container px-6 py-10 mx-auto">
+
+        <div class="mt-8 lg:-mx-6 lg:flex lg:items-center">
+            <img
+                class="object-cover w-full lg:mx-6 lg:w-1/2 rounded-xl h-72 lg:h-96"
+                src="assets/business-graphs-it.jpg"
+                alt="">
+
+            <div class="mt-6 lg:w-1/2 lg:mt-0 lg:mx-6 ">
+                <h1 class="text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-white">Suivez Vos Projets de Recherche avec Précision</h1>
+
+                <p class="mt-3 text-gray-600 dark:text-gray-400 text-justify">
+                    Notre application vous offre une vue complète et détaillée de l'avancement de
+                    vos projets de recherche. Suivez chaque étape du processus, des premières idées
+                    aux résultats finaux. Visualisez les progrès en temps réel, gérez les livrables,
+                    et respectez les échéances grâce à des outils de suivi performants. Que ce soit pour
+                    collaborer avec vos collègues ou pour monitorer l'état d'un projet, notre solution
+                    simplifie la gestion en centralisant toutes les données importantes, vous permettant
+                    de rester concentré sur l'essentiel : la réussite de vos projets.
+                </p>
+
+
+                <div class="flex items-center mt-6">
+                    
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+
