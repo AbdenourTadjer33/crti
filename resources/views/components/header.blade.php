755c484104219bbc8920056e3fd55e8afff2d7f3
AbdenourTadjer33 <abdennour.tadjer@gmail.com>
2024-09-15 03:17:11 +0100

    updating the dashbord link url

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -39,7 +39,7 @@
                     Connexion
                 </button>
             @else
-                <a href="/login"
+                <a href="/app"
                     class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm sm:text-base font-medium cursor-pointer transition-colors focus:outline-none disabled:pointer-events-none disabled:opacity-50 focus:ring h-10 px-4 py-2 text-gray-600 hover:bg-gray-100 hover:text-gray-900 dark:hover:bg-gray-800 dark:hover:text-gray-50 focus:ring-gray-100 dark:focus:ring-gray-800">
                     Dashboard
                 </a>
