ab6a827cd7772a7fe3df0117bd5d8c31a14ecdf1
AbdenourTadjer33 <abdennour.tadjer@gmail.com>
2024-09-20 21:03:50 +0100

    dashboard actions updated

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -6,12 +6,22 @@
                 <img class="w-24" src="/assets/logo.png" alt="logo" />
 
                 <div class="flex gap-2 justify-center md:hidden">
-                    @if (Auth::check())
-                        <a href="/app"
-                            class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm sm:text-base font-medium cursor-pointer transition-colors focus:outline-none disabled:pointer-events-none disabled:opacity-50 focus:ring h-10 px-4 py-2 text-gray-600 hover:bg-gray-100 hover:text-gray-900 dark:hover:bg-gray-800 dark:hover:text-gray-50 focus:ring-gray-100 dark:focus:ring-gray-800">
-                            Dashboard
-                        </a>
-                    @endif
+                    {{-- @auth
+                        @can('application.access')
+                            <a href="/app"
+                                class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm sm:text-base font-medium cursor-pointer transition-colors focus:outline-none disabled:pointer-events-none disabled:opacity-50 focus:ring h-10 px-4 py-2 text-gray-600 hover:bg-gray-100 hover:text-gray-900 dark:hover:bg-gray-800 dark:hover:text-gray-50 focus:ring-gray-100 dark:focus:ring-gray-800">
+                                Dashboard
+                            </a>
+                        @else()
+                            <form method="POST" action="{{ route('logout.destroy') }}">
+                                <button type="submit"
+                                    class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm sm:text-base font-medium cursor-pointer transition-colors focus:outline-none disabled:pointer-events-none disabled:opacity-50 focus:ring h-10 px-4 py-2 text-gray-600 hover:bg-gray-100 hover:text-gray-900 dark:hover:bg-gray-800 dark:hover:text-gray-50 focus:ring-gray-100 dark:focus:ring-gray-800">
+                                    Se déconnecter
+                                </button>
+                            </form>
+                        @endcan
+                    @endauth --}}
+                    <x-dashboard-action />
                 </div>
             </div>
             <div>
@@ -26,11 +36,8 @@
             </div>
         </div>
 
-        @if (Auth::check())
-            <a href="/app"
-                class=" md:inline-flex hidden items-center justify-center whitespace-nowrap rounded-md text-sm sm:text-base font-medium cursor-pointer transition-colors focus:outline-none disabled:pointer-events-none disabled:opacity-50 focus:ring h-10 px-4 py-2 text-gray-600 hover:bg-gray-100 hover:text-gray-900 dark:hover:bg-gray-800 dark:hover:text-gray-50 focus:ring-gray-100 dark:focus:ring-gray-800">
-                Dashboard
-            </a>
-        @endif
+        <div class="md:block hidden">
+            <x-dashboard-action />
+        </div>
     </div>
 </header>
