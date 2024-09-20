23f6c669530f0bd433f7e2d9619e2aea3c265938
AbdenourTadjer33 <abdennour.tadjer@gmail.com>
2024-09-20 16:26:43 +0100

    header responsive test passed

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -5,7 +5,7 @@
 
                 <img class="w-24" src="/assets/logo.png" alt="logo" />
 
-                <div class="flex gap-2 justify-center">
+                <div class="flex gap-2 justify-center md:hidden">
                     @if (Auth::check())
                         <a href="/app"
                             class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm sm:text-base font-medium cursor-pointer transition-colors focus:outline-none disabled:pointer-events-none disabled:opacity-50 focus:ring h-10 px-4 py-2 text-gray-600 hover:bg-gray-100 hover:text-gray-900 dark:hover:bg-gray-800 dark:hover:text-gray-50 focus:ring-gray-100 dark:focus:ring-gray-800">
@@ -25,5 +25,12 @@
                 </p>
             </div>
         </div>
+
+        @if (Auth::check())
+            <a href="/app"
+                class=" md:inline-flex hidden items-center justify-center whitespace-nowrap rounded-md text-sm sm:text-base font-medium cursor-pointer transition-colors focus:outline-none disabled:pointer-events-none disabled:opacity-50 focus:ring h-10 px-4 py-2 text-gray-600 hover:bg-gray-100 hover:text-gray-900 dark:hover:bg-gray-800 dark:hover:text-gray-50 focus:ring-gray-100 dark:focus:ring-gray-800">
+                Dashboard
+            </a>
+        @endif
     </div>
 </header>
