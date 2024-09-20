05ed924b971a85e02c3d1059a532e51314cdac6a
AbdenourTadjer33 <abdennour.tadjer@gmail.com>
2024-09-20 21:01:38 +0100

    update the permission token

--- app/Http/Requests/Project/StoreRequest.php
+++ app/Http/Requests/Project/StoreRequest.php
@@ -14,7 +14,7 @@ class StoreRequest extends FormRequest
     public function authorize(): bool
     {
 
-        return Gate::check('create.projects');
+        return Gate::check('projects.create');
     }
 
     /**
